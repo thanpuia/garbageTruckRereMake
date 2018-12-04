3e41c3b80de883bfc79ad496a497404aad5ab267
Tpa Chhangte <thanpuia46@gmail.com>
2018-12-04 16:10:00 +0530

    emulator cretead

--- app/src/main/java/com/example/lalthanpuiachhangte/garbagetruckreremake/MainActivity.java
+++ app/src/main/java/com/example/lalthanpuiachhangte/garbagetruckreremake/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.lalthanpuiachhangte.garbagetruckreremake;
 
+/*
+
+ ***
+ ***         THIS ACTIVITY IS
+ ***              FOR THE
+ ***            TRUCK DRIVE
+ ***
+
+ */
+
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -21,10 +41,21 @@ import org.w3c.dom.Text;
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
-    TextView latTV,longTV;
+    TextView latTV, longTV;
     LocationManager locationManager;
+    public DatabaseReference latRef, longRef;
+    public Spinner minuteSpinner, secondSpinner;
+
+    Button liveButton;
+    Boolean liveButtonStatus = false;
+
+    public static int minTime;
+    public int second_user,minute_user;
+
+    public LocationListener locationListener;
 
     String TAG = "TAG";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,18 +65,90 @@ public class MainActivity extends AppCompatActivity {
         latTV = findViewById(R.id.latitudeTextView);
         longTV = findViewById(R.id.longitudeTextView);
 
+        minuteSpinner = findViewById(R.id.minuteSpinner);
+        secondSpinner = findViewById(R.id.secondSpinner);
+
+        liveButton = findViewById(R.id.liveButton);
+        ArrayAdapter<CharSequence> adapterSecond =
+                ArrayAdapter.createFromResource(this,R.array.second,android.R.layout.simple_dropdown_item_1line);
+        ArrayAdapter<CharSequence> adapterMinute =
+                ArrayAdapter.createFromResource(this,R.array.minute,android.R.layout.simple_dropdown_item_1line);
+
+        secondSpinner.setAdapter(adapterSecond);
+        minuteSpinner.setAdapter(adapterMinute);
+
+        //THIS WILL TAKE THE SECOND/S FROM THE USER
+        secondSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                second_user = spinnerSecondPositionChecker(position);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) { }});
+
+
+        //THIS WILL TAKE THE MINUTE/S FROM THE USER
+        minuteSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                minute_user = spinnerMinutePositionChecker(position);
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) { }});
+
+
+        //converting secs to millSecs (sec * 1000)
+        //converting minute to millSecs (minute * 60 * 1000)
+
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
-        DatabaseReference latRef = database.getReference("latitude");
-        DatabaseReference longRef = database.getReference("longitude");
+        latRef = database.getReference("latitude");
+        longRef = database.getReference("longitude");
 
-        myRef.setValue("Hello, World!");
+        myRef.setValue("Fighting!");
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
+ /*       //THIS LISTEN TO ANY CHANGEs IN THE GPS COORDINATES
+        LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latRef.setValue(location.getLatitude());
+                longRef.setValue(location.getLongitude());
 
+            }
 
-      /*  myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, 5, locationListener);
+*/
+        //THIS IS NOT VERY IMPORTANT
+        myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
@@ -63,6 +166,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        //THIS LISTEN TO THE VALUE CHANGE FROM THE DATABASE WHICH IS BEING UPDATED BY THE TRUCK DRIVER
         latRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -76,6 +180,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //THIS LISTEN TO THE VALUE CHANGE FROM THE DATABASE WHICH IS BEING UPDATED BY THE TRUCK DRIVER
         longRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -87,11 +192,84 @@ public class MainActivity extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
-        });*/
-
-
-
+        });
 
     }
 
+    public void BackClick(View view) {
+        Intent intent = new Intent(this,MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public int spinnerSecondPositionChecker(int userSelect){
+        int [] seconds = new int[]{0,5,10,20,35,48,55};
+        return seconds[userSelect];
+    }
+
+    public int spinnerMinutePositionChecker(int userSelect){
+        int [] minute = new int[]{0,1,2,3,4,5,7,10,15,20,30,60};
+        return minute[userSelect] ;
+    }
+
+    public void liveButtonClick(View view) {
+
+        minTime = second_user * 1000 + minute_user * 60 * 1000;
+
+        if (liveButtonStatus){
+            liveButton.setTextColor(R.color.colorGrey);
+            liveButtonStatus = false;
+            Toast.makeText(this,"false",Toast.LENGTH_SHORT).show();
+            liveButton.setText("Status: Off");
+            locationManager.removeUpdates(locationListener
+            );
+        }
+        else{
+            liveButton.setTextColor(R.color.colorAccent);
+            liveButtonStatus = true;
+            Toast.makeText(this,"true",Toast.LENGTH_SHORT).show();
+            liveButton.setText("Status: On");
+            listenForLocation();
+        }
+    }
+
+    private void listenForLocation() {
+        //THIS LISTEN TO ANY CHANGEs IN THE GPS COORDINATES
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latRef.setValue(location.getLatitude());
+                longRef.setValue(location.getLongitude());
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, 5, locationListener);
+
+    }
 }
+
--- app/src/main/java/com/example/lalthanpuiachhangte/garbagetruckreremake/MapsActivity.java
+++ app/src/main/java/com/example/lalthanpuiachhangte/garbagetruckreremake/MapsActivity.java
@@ -1,8 +1,20 @@
 package com.example.lalthanpuiachhangte.garbagetruckreremake;
 
+/*
+
+***
+***         THIS ACTIVITY IS
+***           FOR THE USER
+***
+
+ */
+
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -15,12 +27,14 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.support.annotation.NonNull;
 
 import android.util.Log;
 
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -29,17 +43,26 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    public GoogleMap mMap;
     TextView textView;
     TextView latTV, longTV;
+    public DatabaseReference latRef, longRef;
+    public double newLat = 0;
+    public double newLong = 0;
 
-    public int latValue, longValue;
+    public double latValue, longValue;
 
     String TAG = "TAG";
 
     public LocationManager locationManager;
+
+    public boolean countMarker = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,12 +86,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }
 
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        final double longitude = location.getLongitude();
-        double latitude = location.getLatitude();//
-//        latTV = findViewById(R.id.latitudeTextView);
-//        longTV = findViewById(R.id.longitudeTextView);
+       // Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        //locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    /*    final double longitude = location.getLongitude();
+        double latitude = location.getLatitude();//*/
+        latTV = findViewById(R.id.latitudeTextView);
+        longTV = findViewById(R.id.longitudeTextView);
 //
 //        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 //
@@ -89,12 +112,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        double latitude = location.getLatitude();
 //
 //        //CREATING FIREBASE REFERENCE
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
-//        DatabaseReference myRef = database.getReference("message");
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
 //
 //        //CREATING UNIQUE REFERENCE FOR LAT AND LONG
-//        DatabaseReference latRef = database.getReference("latitude");
-//        DatabaseReference longRef = database.getReference("longitude");
+         latRef = database.getReference("latitude");
+         longRef = database.getReference("longitude");
 //
 //
 //        //SET VALUE FOR LATITUTDE AND LONGITUDE
@@ -154,50 +177,136 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        //UPDATING THE MARKER POSITION
 //
 //
-        LocationListener locationListener = new LocationListener() {
+//        LocationListener locationListener = new LocationListener() {
+//            @Override
+//            public void onLocationChanged(Location location) {
+//                //marker.remove();
+//
+//                /*LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+//                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//*/
+//                List<String> providerList = locationManager.getAllProviders();
+//                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+//                String lol="";
+//                try {
+//                    List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+//                    if(null!=listAddresses&&listAddresses.size()>0){
+//                         lol = listAddresses.get(0).getAddressLine(0);
+//                    }
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+//                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//                if(!lol.equals("")) latTV.setText("Latitude: "+lol);
+//
+//
+//            }
+//
+//            @Override
+//            public void onStatusChanged(String provider, int status, Bundle extras) {
+//
+//            }
+//
+//            @Override
+//            public void onProviderEnabled(String provider) {
+//
+//            }
+//
+//            @Override
+//            public void onProviderDisabled(String provider) {
+//
+//            }
+//        };
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 800, 1, locationListener);
+
+       // CREATE LISTENER FOR LATITUDE
+        latRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onLocationChanged(Location location) {
-                //marker.remove();
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                latValue = dataSnapshot.getValue(double.class);
+                latTV.setText("Latitude: "+latValue);
 
-                /*LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
-                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-*/
+                newLat = latValue;
+                if(newLong != 0) {
+                    setMap();
+                }
 
-                LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
-                mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-                
             }
 
             @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        //CREATE LISTENER FOR LONGITUDE
+        longRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                longValue = dataSnapshot.getValue(double.class);
+                longTV.setText("Longitude: "+longValue);
+
+                newLong= longValue;
+                if(newLat != 0) {
+                    setMap();
+                }
 
             }
 
             @Override
-            public void onProviderEnabled(String provider) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 800, 1, locationListener);
+        });
 
 
+    }
+    public MarkerOptions a;
+    public Marker m;
+
+
+    public void setMap() {
+
+       // Marker marker;
+
+     //   LatLng sydney = new LatLng(locat.getLatitude(), location.getLongitude());
+
+        LatLng sydney = new LatLng (newLat, newLong);
+       // LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+         a = new MarkerOptions().position(new LatLng(newLat, newLong));
+         m = mMap.addMarker(a);
+
+       // countMarker = false;
+
+        //mMap.addMarker(new MarkerOptions().position(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+       // m.remove();
+        //m.isDraggable();
+        mMap.clear();
+
+       // m.setPosition(sydney);
+        mMap.addMarker(new MarkerOptions().position(sydney));
+       // m.setPosition(sydney);
+
+
+//        MarkerOptions a = new MarkerOptions().position(new LatLng(newLat, newLong));
+//        Marker m = mMap.addMarker(a);
+//        m.setPosition(new LatLng(newLat, newLong));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        m.remove();
     }
 
 
@@ -216,10 +325,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+       mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
 
 
+    }
+
+    public void GoToDrivePageClick(View view) {
+        Intent intent = new Intent (this, MainActivity.class);
+        startActivity(intent);
     }
 }
